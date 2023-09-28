427edfe6ed2109a82fc8d228a2c608a39dd2af85
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2023-09-28 10:22:48 +0330

    [CHANGE] header layout

--- components/layout/header/index.vue
+++ components/layout/header/index.vue
@@ -1,21 +1,21 @@
 <template>
-  <header class="py-4">
-    <nav class="container flex items-center justify-between">
-      <div class="flex items-center space-x-3">
-        <Icon name="mdi:code-tags" class="text-3xl" />
-        <span class="font-medium">
-          {{ config.title }}
-          <span class="text-blue-700 ml-2 lg:inline hidden">
-            {{ config.slogan }}
-          </span>
-        </span>
-      </div>
+    <header class="py-4">
+        <nav class="container max-w-4xl flex items-center justify-between">
+            <div class="flex items-center space-x-3">
+                <Icon name="mdi:code-tags" class="text-3xl" />
+                <span class="font-medium">
+                    {{ config.title }}
+                    <span class="text-blue-700 ml-2 lg:inline hidden">
+                        {{ config.slogan }}
+                    </span>
+                </span>
+            </div>
 
-      <div>
-        <LayoutHeaderList :items="config.items" />
-      </div>
-    </nav>
-  </header>
+            <div>
+                <LayoutHeaderList :items="config.items" />
+            </div>
+        </nav>
+    </header>
 </template>
 
 <script setup lang="ts">
